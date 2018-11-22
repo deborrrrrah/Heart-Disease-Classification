772fcd4199369dec9b7be46d26a67ac14933ef31
deborrrrrah <dajtpbl@gmail.com>
2018-11-22 20:10:05 +0700

    add predict by model

---
+++ main.py
@@ -0,0 +1,17 @@
+import joblib
+import json
+import sys
+
+filename = sys.argv[1]
+
+data = []
+
+with open(filename) as json_file:
+    x = json.load(json_file)
+    for key, value in x.iteritems():
+        data.append(value)
+
+model = joblib.load('model/model.sav')
+pred = model.predict(data)
+result = pred[0]
+return result
