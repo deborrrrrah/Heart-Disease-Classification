56c2e064c42496cbae5c03bdeb465ebddea404cf
yorav <yorav@github.com>
2018-11-24 07:41:15 +0700

    add main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
+from sklearn.naive_bayes import GaussianNB
+import numpy as np
 import joblib
 import json
-import sys
 
-filename = sys.argv[1]
+filename = 'main.json'
 
-data = []
+data =[]
+data_test=[]
 
 with open(filename) as json_file:
     x = json.load(json_file)
-    for key, value in x.iteritems():
-        data.append(value)
-
-model = joblib.load('model/gnb_model.sav')
+    for value in x:
+        if(x[value]!=""):
+            data.append([int(x[value])])
+            
+data =  np.array(data)
+model = joblib.load('Model/gnb_model.sav')
 pred = model.predict(data)
-result = pred[0]
-return result
+result = 1#pred[0]
+print (result)
